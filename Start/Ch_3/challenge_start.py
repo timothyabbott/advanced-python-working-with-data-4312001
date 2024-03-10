ac9dd5d29bf78f77720cb3d2dc1c9fdfb6564e69
timothyabbott <20151421+timothyabbott@users.noreply.github.com>
2024-03-10 19:53:55 +0000

    completed serialisation

--- Start/Ch_3/challenge_start.py
+++ Start/Ch_3/challenge_start.py
@@ -3,13 +3,31 @@
 
 import json
 import csv
+import datetime
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
 
 # Create a CSV file with the following information:
 # 40 most significant seismic events, ordered by most recent
 # Header row: Magnitude, Place, Felt Reports, Date, and Google Map link
 # Date should be in the format of YYYY-MM-DD
+    
+top_40_events = sorted(data['features'],key= lambda feature:feature['properties']['sig'],reverse=True)[:40]
+top_40_events_sorted_by_date = sorted(top_40_events,key=lambda feature:feature['properties']['time'],reverse=True)
+
+header = ['Magnitude', 'Place', 'Felt Reports', 'Date','Google Map link']
+
+with open('events.csv','w') as events_file:
+    writer = csv.writer(events_file,delimiter=',')
+    for event in top_40_events:
+        writer.writerow([
+            event['properties']['mag'],
+            event['properties']['place'],
+            event['properties']['felt'] if event['properties']['felt'] else '',
+            str(datetime.date.fromtimestamp(event['properties']['time']/1000)),
+            f"https://www.google.com/maps/@?api=1&map_action=map&center={event['geometry']['coordinates'][1]}%2C{event['geometry']['coordinates'][0]}&zoom=12&basemap=terrain"
+        ])
+
--- Start/Ch_3/deserialize.py
+++ Start/Ch_3/deserialize.py
@@ -3,10 +3,26 @@
 
 import csv
 import pprint
-
+result = []
 
 # read the contents of a CSV file into an object structure
-result = []
+with open('largequakes.csv','r') as csv_for_reading:
+    reader = csv.reader(csv_for_reading)
+    sniffer = csv.Sniffer()
+    sample = csv_for_reading.read(1024)
+    csv_for_reading.seek(0)
+
+    if sniffer.has_header(sample=sample):
+        next(reader)
+    for row in reader:
+       result.append({
+          "place": row[0],
+          "magnitude": row[1],
+          "date": row[2],
+          "link": row[3]
+       })
+        
+
 
 # TODO: open the CSV file for reading
 
